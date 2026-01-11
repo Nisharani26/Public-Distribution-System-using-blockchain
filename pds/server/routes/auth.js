e734f40c5a425bec972c1e522ce064a6251b492e
Nisharani26 <nisharani262004@gmail.com>
2026-01-11 18:29:22 +0530

    recent changes

--- pds/server/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { authorityLogin } = require("../controllers/authController");
-
-router.post("/authority/login", authorityLogin);
-
-module.exports = router;
